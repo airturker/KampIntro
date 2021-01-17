5568d936cbef5fe08d300184fef793b92c6315dc
Mehmet Türker ÖZAKINCI <airtur@hotmail.com>
2021-01-17 03:19:38 +0300

    Add .gitignore and .gitattributes.

---
+++ Donguler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //array - dizi
+
+            string[] kurslar = new string[] 
+            { 
+                "yazılım geliştirici yetiştirme kampı ", "programlamaya başlangıç için temel kurs", "java kursu", "python", "c#"
+            };
+
+
+            for (int i = 0; i<kurslar.Length; i++)    // (başlangıç;şart;artış)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("-for bitti-");
+
+            foreach (string kurs in kurslar)    // foreach dizilere uygulanır
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1, urun2 };
+
+            //type-safe -- tip güvenliği
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-----------------------");
+            }
+
+            Console.WriteLine("------metotlar--------");
+            //instance - örnek 
+            //encapsulation
+
+            SepetManager sepetmanager = new SepetManager(); //metodu çağır ve tanımla
+            sepetmanager.Ekle(urun1);
+            sepetmanager.Ekle(urun2);
+            
+            //sepetManager.Ekle2("armut", "yeşil armut", 12, 10);
+            //sepetManager.Ekle2("muz", "çikita", 20, 7);
+            
+        }
+    }
+}
+
+//do not repeat yourself - DRY - Clean Code - Best Practice
