83c8caa28801874bbc6b678d47e88bd52610e98a
Mehmet Türker ÖZAKINCI <airtur@hotmail.com>
2021-01-30 10:56:55 +0300

    sql li yedek

---
+++ sqlödev29012021.sql
@@ -0,0 +1,5 @@
+﻿Select p.ProductName AS 'Ürün Adı', 
+SUM(od.Quantity * od.UnitPrice) AS 'Kazanılan Toplam Miktar'
+from Products p inner join [Order Details] od on p.ProductID = od.ProductId 
+inner join Orders o on od.OrderID = o.OrderID
+group by p.ProductName
